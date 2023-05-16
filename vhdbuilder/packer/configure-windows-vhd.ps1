f195185a3690ea0cca3d8d0dd1a431bd7b2385b1
Junjie Zhang <junjiezhang@microsoft.com>
2023-05-16 07:25:13 +0000

    test

--- vhdbuilder/packer/configure-windows-vhd.ps1
+++ vhdbuilder/packer/configure-windows-vhd.ps1
@@ -541,7 +541,7 @@ try{
             Set-WinRmServiceDelayedStart
             Update-DefenderSignatures
             Install-WindowsPatches
-            Install-OpenSSH
+            # Install-OpenSSH
             Update-WindowsFeatures
         }
         "2" {
